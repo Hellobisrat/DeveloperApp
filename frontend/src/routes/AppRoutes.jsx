e6eba26662864885e9ee5dc408236322d22f23dc
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-10 19:30:11 -0500

    Protected Route added

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,13 +4,29 @@ import Home from "../pages/Home";
 import Post from "../pages/Post";
 import Edit from "../pages/Edit";
 
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+
+import ProtectedRoute from "./ProtectedRoute";
+
 export default function AppRoutes() {
   return (
     <Routes>
-        <Route path="/login" element={<Login />} />
+      {/* Public Auth Routes */}
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/" element={<Layout />}>
+
+      {/* Protected Routes */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
         <Route index element={<Home />} />
         <Route path="post" element={<Post />} />
         <Route path="edit/:id" element={<Edit />} />
