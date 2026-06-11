38f9646d8fb1a4fad3b5bef9291f8f0c42d37280
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-06-10 21:55:05 -0500

    back to the orginal api

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "http://localhost:3000",
-    "https://precious-lily-1fb884.netlify.app"
+    "http://localhost:3000"
   ],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://your-backend.onrender.com",
+baseURL: "http://localhost:9000/api",
   withCredentials: true,
 });
 
