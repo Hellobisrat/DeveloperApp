71f2fad556700c469c888576c0fc2da9018f8710
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-07-23 15:32:41 -0500

    add docker build workflow

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,7 @@ import mongoose from "mongoose"
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      'mongodb://user:password@developerapp-mongodb:27017/developerapp?authSource=admin'
-    );
+    await mongoose.connect('mongodb://user:password@developerapp-mongodb:27017/developerapp?authSource=admin');
     console.log('db connected');
   } catch (error) {
     console.log(error);
