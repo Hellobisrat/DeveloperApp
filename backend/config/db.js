c1c1d1486101a2be0dac46a4d408e1b64be59698
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-07-23 15:52:52 -0500

    update on mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,16 @@
-import mongoose from "mongoose"
-
-
-
+import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect('mongodb://user:password@developerapp-mongodb:27017/developerapp?authSource=admin');
+    await mongoose.connect(
+      'mongodb://user:password@mongodb:27017/developerapp?authSource=admin'
+    );
     console.log('db connected');
   } catch (error) {
     console.log(error);
-    process.exit(1);
   }
 };
 
 export default dbConnect;
+
 
