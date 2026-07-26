3a4aeee5738764d6cc47b57cd544926f130da20d
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-07-26 17:48:03 -0500

    .env added in the dir

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
+const username = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PWD;
+const dbName = process.env.MONGO_DB_NAME;
+const host = process.env.MONGO_DB_HOST;
+const port = process.env.MONGO_DB_PORT;
+
+const uri = `mongodb://${username}:${password}@${host}:${port}/${dbName}?authSource=admin`;
+
+
 const dbConnect = async () => {
   try {
-    await mongoose.connect(
-      'mongodb://user:password@mongodb:27017/developerapp?authSource=admin'
-    );
+    await mongoose.connect(uri );
     console.log('db connected');
   } catch (error) {
     console.log(error);
